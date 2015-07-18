0e021d0c29c17e73b274966af270306d32ff2dbd
Holloway, Kean Ho <hollowaykeanho@gmail.com>
2015-07-19 02:33:43 +0800

    added formspree js for sending email
    Signed-off-by: Holloway, Kean Ho <hollowaykeanho@gmail.com>

---
+++ assets/js/formspree.js
@@ -0,0 +1,40 @@
+(function($){
+	$(document).ready(function() {
+		/* ---------------------------------------------- /*
+		 * E-mail validation
+		/* ---------------------------------------------- */
+
+		function isValidEmailAddress(emailAddress) {
+			var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+			return pattern.test(emailAddress);
+		};
+
+		/* ---------------------------------------------- /*
+		 * Contact form ajax
+		/* ---------------------------------------------- */
+
+		$('#contact-form').submit(function(e) {
+			e.preventDefault();
+			var response = $('#contact-form .ajax-response');
+			$('#contact-form .ajax-hidden').fadeOut(500);
+			$.ajax({
+				url: '//formspree.io/hollowaykeanho@gmail.com',
+				method: 'POST',
+				data: $(this).serialize(),
+				dataType: 'json',
+				beforeSend: function() {
+					response.html("Sending Message...").fadeIn(500);
+				},
+				success: function(data) {
+					response.html("Message Sent. I will be contacting you asap. Thank You.").fadeIn(500);
+				},
+				error: function(err) {
+					response.html("Looks like there was an error! Please refresh the page.").fadeIn(500);
+				}
+			});
+            return false;
+		});
+
+	});
+
+})(jQuery);
